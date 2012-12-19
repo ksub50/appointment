c8b7e5ab04b7d7ddfe37bc934858d62f47e00a65
ksub <ksub@192.168.2.3>
2012-12-18 23:22:46 -0800

    Added vacation and actual salary drawn.

--- appointment/src/com/kuttan/client/Appointment.java
+++ appointment/src/com/kuttan/client/Appointment.java
@@ -2,6 +2,8 @@ package com.kuttan.client;
 
 import java.util.Date;
 
+import sun.rmi.runtime.NewThreadAction;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.event.dom.client.BlurEvent;
@@ -16,9 +18,10 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.sun.corba.se.impl.ior.NewObjectKeyTemplateBase;
 
 public class Appointment implements EntryPoint {
-	public void onModuleLoad( ) {
+	public void onModuleLoad() {
 		Label nameLabel = new Label();
 		nameLabel.setText("Enter Name");
 		final TextBox nameTextbox = new TextBox();
@@ -26,30 +29,30 @@ public class Appointment implements EntryPoint {
 		Label dateLabel = new Label();
 		dateLabel.setText("Enter Date of Joining");
 		final TextBox dateTextbox = new TextBox();
-		
+
 		Label datehelp = new Label("dd/mm/yyyy");
-		
+
 		Label basicLabel = new Label();
 		basicLabel.setText("Enter Basic Salary in Rs/pp ");
 		final TextBox basicTextbox = new TextBox();
-		
+
 		Label basichelp = new Label("rs/pp");
-		
+
 		Label daLabel = new Label("Enter Dearness Allowance in Rs/pp");
 		final TextBox daTextbox = new TextBox();
-		
+
 		Label hraLabel = new Label();
 		hraLabel.setText("House Rent Allowance 30% Basic");
 		final TextBox hraTextbox = new TextBox();
-		
+
 		Label totalLabel = new Label("Total Salary in Rs/pp");
 		final TextBox totalsalaryTextbox = new TextBox();
 
-		basicTextbox.addBlurHandler(new BlurHandler() {			
+		basicTextbox.addBlurHandler(new BlurHandler() {
 			@Override
 			public void onBlur(BlurEvent event) {
-				//Get the value from basic text
-				//hra value = 03 * basic Text
+				// Get the value from basic text
+				// hra value = 03 * basic Text
 				String basicValue = basicTextbox.getText();
 				Double basicDoubleValue = Double.parseDouble(basicValue);
 				Double hraDoubleValue = 0.3 * basicDoubleValue;
@@ -57,21 +60,86 @@ public class Appointment implements EntryPoint {
 				hraTextbox.setText(hraValue);
 			}
 		});
-         
-        daTextbox.addBlurHandler(new BlurHandler() {
+
+		daTextbox.addBlurHandler(new BlurHandler() {
 			@Override
 			public void onBlur(BlurEvent event) {
-			String basicValue = basicTextbox.getText();
-			Double basicDoubleValue = Double.parseDouble(basicValue);
-			Double hraDoubleValue = 0.3 * basicDoubleValue;
-			String daValue = daTextbox.getText();
-			Double daDoubleValue = Double.parseDouble(daValue);
-			
-			Double totalsalaryDoubleValue = basicDoubleValue+daDoubleValue+hraDoubleValue;
-			String totalsalaryValue = String.valueOf(totalsalaryDoubleValue);
-			totalsalaryTextbox.setText(totalsalaryValue);
+				String basicValue = basicTextbox.getText();
+				Double basicDoubleValue = Double.parseDouble(basicValue);
+				Double hraDoubleValue = 0.3 * basicDoubleValue;
+				String daValue = daTextbox.getText();
+				Double daDoubleValue = Double.parseDouble(daValue);
+
+				Double totalsalaryDoubleValue = basicDoubleValue
+						+ daDoubleValue + hraDoubleValue;
+				String totalsalaryValue = String
+						.valueOf(totalsalaryDoubleValue);
+				totalsalaryTextbox.setText(totalsalaryValue);
+			}
+
+		});
+		final Label replyLabel = new Label("reply");
+
+		Button saveButton = new Button("Save");
+		// /========================
+		saveButton.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				if (nameTextbox.getText().length() >0 &&
+						dateTextbox.getText().length() > 0 &&
+						totalsalaryTextbox.getText().length() > 0) {
+					replyLabel.setText("Saved");
+				} else {
+					replyLabel.setText("Enter Details");
+				}
 			}
 		});
+		
+		Label WorkingDaysLabel = new Label("No.of Workingdays in the month");
+		final TextBox WorkingDaysTextbox = new TextBox();
+		
+		Label WorkedDaysLabel = new Label("No of Days Worked in the month");
+		final TextBox WorkedDaysTextbox = new TextBox();
+		
+		Label SalaryforthemonthLabel = new Label("Salary Earned for the month");
+		final TextBox SalaryforthemonthTextbox = new TextBox();
+		
+		WorkedDaysTextbox.addBlurHandler(new BlurHandler() {
+			
+			@Override
+			public void onBlur(BlurEvent event) {
+				String WorkingdaysValue = WorkingDaysTextbox.getText();
+				Double WorkingdaysDoubleValue = Double.parseDouble(WorkingdaysValue);
+				String WorkeddaysValue = WorkedDaysTextbox.getText();
+				Double WorkeddaysDoubleValue = Double.parseDouble(WorkeddaysValue);
+				
+				String totalsalaryValue = totalsalaryTextbox.getText();
+				Double totalsalaryDoubleValue = Double.parseDouble(totalsalaryValue);
+				Double SalaryforthemonthDoubleValue = (totalsalaryDoubleValue/WorkingdaysDoubleValue)*WorkeddaysDoubleValue;
+				String SalaryforthemonthValue = String
+						.valueOf(SalaryforthemonthDoubleValue);
+				SalaryforthemonthTextbox.setText(SalaryforthemonthValue);
+				
+			}
+		});
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+				
+		
+		// /========================
 		VerticalPanel vp1 = new VerticalPanel();
 		vp1.add(nameLabel);
 		vp1.add(dateLabel);
@@ -80,7 +148,12 @@ public class Appointment implements EntryPoint {
 		vp1.add(hraLabel);
 		vp1.add(totalLabel);
 		vp1.setSpacing(20);
-		
+		vp1.add(WorkingDaysLabel);
+		vp1.add(WorkedDaysLabel);
+		vp1.add(SalaryforthemonthLabel);
+		vp1.add(saveButton);
+		vp1.add(replyLabel);
+
 		VerticalPanel vp2 = new VerticalPanel();
 		HorizontalPanel dateHelpPanel = new HorizontalPanel();
 		dateHelpPanel.add(dateTextbox);
@@ -93,74 +166,34 @@ public class Appointment implements EntryPoint {
 		vp2.add(hraTextbox);
 		vp2.add(totalsalaryTextbox);
 		vp2.setSpacing(10);
-		
+		vp2.add(WorkingDaysTextbox);
+		vp2.add(WorkedDaysTextbox);
+		vp2.add(SalaryforthemonthTextbox);
+
 		VerticalPanel vp3 = new VerticalPanel();
 		vp3.add(basichelp);
 		vp3.setSpacing(80);
-		
+
 		HorizontalPanel hp1 = new HorizontalPanel();
 		hp1.add(vp1);
 		hp1.add(vp2);
 		hp1.add(vp3);
-		
-	
-		
-
-		Label timeLabel = new Label();
-		timeLabel.setText("Enter Appointment Time ");
-		final TextBox timeTextbox = new TextBox();
-		
-		Button answerButton = new Button();
-		answerButton.setText("Save");
-		final Label replyLabel = new Label();
-
-		ClickHandler handler = new ClickHandler() {
-			@Override
-			public void onClick(ClickEvent event) {
-				String value = dateTextbox.getText();
-				if (value.isEmpty()) {
-					replyLabel.setText("Enter a valid date.");
-				} else {
-					try {
-						DateTimeFormat dateParser = DateTimeFormat.getFormat(PredefinedFormat.DATE_SHORT);
-						Date d = dateParser.parse(value);
-						replyLabel.setText("You entered " + d.toString());
-					} catch(Exception e) {
-						replyLabel.setText("Error parsing date " + e.toString());
-					}
-				}
-			}
-		};
-		answerButton.addClickHandler(handler);
 
 		/*
-		VerticalPanel vp1 = new VerticalPanel();
-		vp1.add(nameLabel);
-		vp1.add(ageLabel);
-		vp1.add(dateLabel);
-		vp1.add(timeLabel);
-		vp1.setSpacing(15);
-		
-		VerticalPanel vp2 = new VerticalPanel();
-		vp2.add(nameTextbox);
-		vp2.add(ageTextbox);
-		vp2.add(dateHelpPanel);
-		vp2.add(timeTextbox);
-		vp2.setSpacing(5);
-		HorizontalPanel hp1 = new HorizontalPanel();
-		hp1.add(vp1);
-		hp1.add(vp2);
-		
-		HorizontalPanel hp2 = new HorizontalPanel();
-		hp2.add(answerButton);
-		hp2.add(replyLabel);
-		
-		VerticalPanel vp3 = new VerticalPanel();
-		vp3.add(hp1);
-		vp3.add(hp2);
-		
-		
-		*/
+		 * VerticalPanel vp1 = new VerticalPanel(); vp1.add(nameLabel);
+		 * vp1.add(ageLabel); vp1.add(dateLabel); vp1.add(timeLabel);
+		 * vp1.setSpacing(15);
+		 * 
+		 * VerticalPanel vp2 = new VerticalPanel(); vp2.add(nameTextbox);
+		 * vp2.add(ageTextbox); vp2.add(dateHelpPanel); vp2.add(timeTextbox);
+		 * vp2.setSpacing(5); HorizontalPanel hp1 = new HorizontalPanel();
+		 * hp1.add(vp1); hp1.add(vp2);
+		 * 
+		 * HorizontalPanel hp2 = new HorizontalPanel(); hp2.add(answerButton);
+		 * hp2.add(replyLabel);
+		 * 
+		 * VerticalPanel vp3 = new VerticalPanel(); vp3.add(hp1); vp3.add(hp2);
+		 */
 		RootPanel.get().add(hp1);
 	}
 }
